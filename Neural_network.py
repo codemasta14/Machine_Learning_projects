067606ea3fc63b86f49c18f31d5225c2f9e73d56
codemasta14 <codemasta14@gmail.com>
2019-03-29 13:40:45 -0600

    s
    .

--- Neural_network.py
+++ Neural_network.py
@@ -5,13 +5,16 @@ Created on Sat Feb 16 14:19:16 2019
 @author: codyj
 """
 
-import numpy as np
-import random
-import pandas as pd
+importndas as pd
+from math import exp
 from sklearn import datasets
+import re
+def sigmoid(x):
+    return(1/(1+exp(-x))) numpy as np
+import random
+import pa
 iris = datasets.load_iris()
 
-    
 
 wow1 = pd.DataFrame(iris.data,columns  = ["a","b","c","d"])
 
@@ -27,64 +30,98 @@ random.seed = 3
 
 #Creates Node class
 class node():
-    def __init__(self,data,bias = False):
-        self.threshhold=.5
-        self.weights = []                  
-        for i in range(len(data[1])):    
-           self.weights.append(random.uniform(-.1,.1))
+    def __init__(self,weights):
+        self.weights = []
+        if data.ndim > 1:
+                self.datas= np.concatenate((data,np.ones((len(data),1))*-1),axis=1)
+                for i in range(len(self.datas[1])):    
+                    self.weights.append(random.uniform(-.1,.1))
+        else:
+                self.datas= np.append(data,-1)
+                for i in range(len(self.datas)):    
+                    self.weights.append(random.uniform(-.1,.1))
+                          
+    def output(self,data):
+        return(np.dot(self.datas,self.weights))
         
-        self.datas= data
-        
-        if bias :
-            self.datas = np.zeros_like(self.datas)
-            self.weights = np.zeros_like(self.weights)-1
-            self.threshhold = 0
-     
-        
-        
-    def fire(self,threshhold=.5):
-        self.threshhold = threshhold
-        self.input = np.dot(self.datas,self.weights)
-        predictions = []
-        for i in range(len(self.input)):
-            
-            if self.input[i] >=self.threshhold:
-                predictions.append(1)
+    def activate(self,data):
+            if self.output(data).ndim > 0:
+                return(list(map(sigmoid,self.output(data))))
             else:
-                predictions.append(0)
-        return(predictions)
-        
-    def adjust_weights(self):
-        pass
-        
-        
-        
-        
+                return(sigmoid(self.output(data)))
 #Creates Neural_net class, which consists of nodes.
+            
 class neural_net():
-    def __init__(self,data,nodes=5):
-        self.nodes = []
-        #Creates list of nodes
-        for i in range(nodes):
-            self.nodes.append(node(data))
-        #Creates bias node
-        self.nodes.append(node(data,bias = True))
-    
-    def predict(self,data,threshhold=.5):
-        predictions = []
-        for k in range(len(data)):
-            node_pred = []
-            for i in self.nodes:
-                node_pred.append(i.fire(threshhold)[k])
-            predictions.append(int(np.mean(node_pred)>=.5))
-        return(predictions)    
+    def __init__(self,data,nodes=5,hidden_layers = 1,classification = True):                      
+        self.nodes = nodes
+        self.hidden_layers = hidden_layers
+        self.classification = classification
+
+        self.layer_list = {}
+        self.data = {}
+        for x in range(self.hidden_layers):
+            self.layer_list["layer{}".format(x)]={}
+            if x == 0:
+                if data.ndim > 1:
+                    self.data["input{}".format(x)] = data[0]
+                else:
+                    self.data["input{}".format(x)] = data
+            else:
+                self.data["input{}".format(x)] =[]
+                for snakes in range(self.nodes):
+                    self.data["input{}".format(x)].append(self.layer_list["layer{}".format(x-1)]["node{}".format(snakes)].output(self.data["input{}".format(x-1)]))
+            self.data["input{}".format(x)]=np.array(self.data["input{}".format(x)])
+            for i in range(self.nodes):
+                
+                self.layer_list["layer{}".format(x)]["node{}".format(i)] = node(self.data["input{}".format(x)])
+        self.data["input{}".format(x+1)]=[]
+        self.layer_list["output"] = {}
         
+        #Creates final input for the last nodes
+        for love in range(self.nodes):
+                
+            self.data["input{}".format(x+1)].append(self.layer_list["layer{}".format(x)]["node{}".format(love)].output(self.data["input{}".format(x)]))
+        self.data["input{}".format(x+1)]=np.array(self.data["input{}".format(x+1)])
+        self.output={}
+        
+        for disaster in range(3):   
+            self.layer_list["output"]["node{}".format(disaster)] = node(self.data["input{}".format(x+1)])
+            self.output["node{}".format(disaster)] = self.layer_list["output"]["node{}".format(disaster)].activate(self.data["input{}".format(x+1)])
+    
     def train(self,data):
-        pass
+        predictions = []
+        for i in len(data):
+            for layer in self.layer_list
+                print(layer)
+                    
+            
+        
+        
+        
+        
+    def predict(self,data):
+         predictions = []
+         for n in data:
+             for nodes in self.nodes
+             
+             
+             
+ #            predictions.append((int(re.search("\d",max(self.output)).group(0))))
+        
+        
 
-cody = node(wow)
-cody.fire()
+cody = node(wow[0])
+cody.output(wow[0])
+cody.activate(wow[0])
+keagan = neural_net(wow[0:2],5,3)
 
-emma = neural_net(wow)
+keagan.predict(wow[0])     
+keagan.output
+wow[0:2]
+keagan.train(wow[2])
+keagan.predict(wow[0:2])
+keagan.output
 
-emma.predict(wow)
+list(map(sigmoid,np.array([1,2,3])))
+list(map(max,keagan.output))
+
---
+++ neural_net_3.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 27 16:45:08 2019
+
+@author: codyj
+"""
+import pandas as pd
+from math import exp
+from sklearn import datasets
+import re
+import numpy as np
+import random
+from sklearn.metrics import confusion_matrix
+def accuracy(y_test,predicted):
+    right = 0
+    cm = confusion_matrix(y_test,predicted)    
+    for i in range(cm.shape[0]):
+        right += cm[i,i]
+        
+    total = sum(sum(cm))
+    print("{}%".format(right/total))
+    return(right/total)
+    
+    
+def sigmoid(x):
+    return(1/(1+exp(-x))) 
+
+
+iris = datasets.load_iris()
+wow1 = pd.DataFrame(iris.data,columns  = ["a","b","c","d"])
+
+
+#standardize data
+wow1.a = ((wow1.a-np.mean(wow1.a))/np.std(wow1.a))
+wow1.b = ((wow1.b-np.mean(wow1.b))/np.std(wow1.b))
+wow1.c = ((wow1.c-np.mean(wow1.c))/np.std(wow1.c))
+wow1.d = ((wow1.d-np.mean(wow1.d))/np.std(wow1.d))
+
+wow = wow1.values
+random.seed = 3
+y = np.array(iris.target)
+
+
+class node():
+    def __init__(self,weights):
+        self.weights = []
+        for i in range(weights+1):    
+            self.weights.append(random.uniform(-.1,.1))
+        self.weights = np.array(self.weights)
+
+                          
+    def output(self,data):
+        if data.ndim >1:
+            data = np.concatenate((data,np.ones((len(data),1))*-1),axis=1)
+        else:
+            data = np.append(data,-1)
+        return(np.dot(data,self.weights))
+        
+    def activate(self,data):
+            if self.output(data).ndim > 0:
+                return(list(map(sigmoid,self.output(data))))
+            else:
+                return(sigmoid(self.output(data)))
+                
+
+
+class neural_net():
+    def __init__(self,weights,hidden_layers = 3,nodes = 3, outputs = 1, classification = True):
+        self.classification = classification
+        self.layer_list = {}
+        
+        for layer in range(hidden_layers):
+            self.layer_list["layer{}".format(layer)] ={}
+            
+            for nubs in range(nodes):
+                if layer == 0:
+                    self.layer_list["layer{}".format(layer)]["node{}".format(nubs)] = node(weights)
+                else:
+                    self.layer_list["layer{}".format(layer)]["node{}".format(nubs)] = node(nodes)
+        self.layer_list["output"]={}
+        for outs in range(outputs):
+            self.layer_list["output"]["node{}".format(outs)] = node(nodes)
+                
+    def train(self,data,y,iterations,learn_rate):
+        if data.ndim ==1:
+            times = 1
+            inputs = data
+            target = y
+            
+        else:
+            times = data.shape[0]
+            inputs = data[n]
+            target = y[n]
+        
+        for n in range(times):
+               
+               for i, outputs in enumerate(self.layer_list['output']):
+                   activations = self.predict(inputs,False)
+                   temp = []
+                   temp_activation = []
+                   for key, value in activations.items():
+                       temp = [key,value]
+                       temp_activation.append(temp)
+                   activation = temp_activation[i][1]
+                   dj = activation*(1-activation)*(activation - target)
+                   
+                   self.layer_list['output'][outputs].weights = self.layer_list['output'][outputs].weights - learn_rate*dj*activation
+               
+    
+    def predict(self,inputs,outputs = True):
+        
+        classes= []
+        if inputs.ndim ==1:
+            times = 1
+        else:
+            times = inputs.shape[0]
+        
+        for n in range(times):
+            if inputs.ndim != 1:
+                data = inputs[n]
+            else:
+                data = inputs
+                
+            for layer in self.layer_list:
+                if layer != "output":
+                    self.layer_list[layer]["output"] = []
+                    for i, node in enumerate(self.layer_list[layer]):
+                        if node != 'output':
+                            self.layer_list[layer]["output"].append(self.layer_list[layer][node].activate(data))
+                    data = np.array(self.layer_list[layer]["output"])
+                    
+              
+                else:
+                    self.layer_list[layer]["output"] = {} 
+                    for i, node in enumerate(self.layer_list[layer]):
+                        if node != "output":
+                            self.layer_list[layer]["output"][node] = self.layer_list[layer][node].activate(data)
+
+            if len(self.layer_list["output"]["output"]) ==1:
+                if self.layer_list["output"]["output"]["node0"] <.5:
+                    classes.append(0)
+                else:
+                    classes.append(1)
+            else:
+                classes.append(int(re.search("\d",max(self.layer_list["output"]["output"])).group(0)))
+        cam = self.layer_list['output']['output']
+        self.layer_list["output"].pop('output')
+        if outputs:
+            return(classes)
+            
+        else:
+            return(cam)
+            
+        
+        
+
+
+cody = neural_net(4,1,1,3)
+cody.predict(wow[2],False)
+
+for i in range(3):
+    print(cody.layer_list['output']['node{}'.format(i)].weights)
+
+cody.train(wow[1],y[1],1,1)
+
+for i in range(3):
+    print(cody.layer_list['output']['node{}'.format(i)].weights)
+    
+accuracy(cody.predict(wow),y)
+
+cody.layer_list['output']
+s={}
+
+s['cody'] = 3
+s['cody2'] = 3
+s.keys()
+
+s.values()
+s.items()
+
+temp = []
+dictlist = []
+for key, value in cody.predict(wow[2],False).items():
+    temp = [key,value]
+    dictlist.append(temp)
+    
+dictlist
+wow = [] 
+for v in s.values():
+    wow.append(v)
